35d7495e1beb75858ca5cfa7b62b19745f15bba9
messa <mesfinmetta@gmail.com>
2023-01-24 18:40:01 -0800

    task 0 again

--- _printf.c
+++ _printf.c
@@ -1,6 +1,5 @@
 #include "main.h"
-#include <stddef.h>
-#include <stdarg.h>
+
 void print_buffer(char buffer[], int *buff_ind);
 
 /**
@@ -12,7 +11,7 @@ int _printf(const char *format, ...)
 {
 	int i, printed = 0, printed_chars = 0;
 	int flags, width, precision, size, buff_ind = 0;
-	va_list list;
+ 	va_list list;
 	char buffer[BUFF_SIZE];
 
 	if (format == NULL)
